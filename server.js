97116d8a8243ecc79b3c09b03f4a3983148cb16a
Amy Rombuk <arombuk@gmail.com>
2020-11-19 09:45:52 +1000

    Added 404 handling to server.js and use of routes.

--- server.js
+++ server.js
@@ -1,11 +1,14 @@
 const express = require('express')
 const mongoose = require('mongoose')
 
+const routes = require('./routes/routes')
+
 const app = express()
 
 app.use(express.urlencoded({extended:true}))
 app.use(express.json())
 
+////////////////// Mongo DB //////////////////////////////////
 mongoose.connect('mongodb+srv://user_grp:switchMaven980@smbootcamp2020.dw1al.gcp.mongodb.net/mac-app-db?retryWrites=true&w=majority', {useNewUrlParser: true, useUnifiedTopology: true}, 
     function(err, database){
         if(err){
@@ -15,6 +18,16 @@ mongoose.connect('mongodb+srv://user_grp:switchMaven980@smbootcamp2020.dw1al.gcp
     }
 )
 
+/////////////////// ROUTES /////////////////////////////////
 
+app.use(routes)
+
+//////////////////// 404 Handling //////////////////////////
+
+app.all('*', function(req, res){
+    //create a page for this
+    res.send("<h1> Sorry the page you requested is not found </h1>")
+})
+///////////////////////////////////////////////////////////////
 
 app.listen(3000, () => console.log("app listening at http://localhost:3000"))
