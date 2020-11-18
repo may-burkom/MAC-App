636fc8b6fba55e27f511dc86ef0974035ea7506d
Dean Peter <deanpeter@switchmaven.com>
2020-11-17 22:06:16 -0800

    appointment model added

---
+++ models/Appointment.js
@@ -0,0 +1,37 @@
+const mongoose = require('mongoose')
+
+const AppointmentSchema = mongoose.Schema({
+    patientCode:{ // to be referenced
+        type:String,
+        unique:true,
+        required: true
+    },
+    PatientName:{
+        type:String,
+        unique:true,
+        required:true
+    },
+    Date:{
+        type:String,
+        required:true
+    },
+    Room:{
+        type:String,
+        required:true
+    },
+    DoctorsID:{    // to be referenced
+        type:String,
+        required:true
+    },
+    DoctorsName:{
+        type:String,
+        required:true
+    },
+    Reasons:{
+        type:String,
+        required:true
+})
+
+const AppointmentModel = mongoose.model('appointment',AppointmentSchema)
+
+module.exports = userModel
